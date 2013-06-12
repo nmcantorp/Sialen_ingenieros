fee0370245e3cf3ccf9dd7ad4c79658ffadfc6d6
Mauricio Cantor <topomajor1@hotmail.com>
2013-06-12 17:18:29 -0500

    Ajuste estilos

--- www/applications/sialen/controllers/sialen.php
+++ www/applications/sialen/controllers/sialen.php
@@ -4,26 +4,27 @@
  */
 
 
-class Default_Controller extends ZP_Controller {
+class Sialen_Controller extends ZP_Controller {
 	
 	public function __construct() {
-		$this->app("default");
+		$this->app("sialen");
 		
 		$this->Templates = $this->core("Templates");
 
 		$this->Templates->theme();
-
 		#$this->Default_Model = $this->model("Default_Model");
 	}
 	
 	public function index() {	
+		$this->Templates->CSS('default', 'default', TRUE);
 		$vars["message"] = __(__("Hello World"));
 		$vars["view"]	 = $this->view("show", TRUE);
 		
-		$this->render("content", $vars);
+		//$this->render("content", $vars);
+		$this->view("show", $vars);
 	}
 
-	public function test($param1 = "Hola", $param2 = "Adios") {
+	/*public function test($param1 = "Hola", $param2 = "Adios") {
 		print "New dispatcher it's works fine: $param1, $param2";
 	}
 
@@ -34,5 +35,5 @@ class Default_Controller extends ZP_Controller {
 		$this->render("content", $vars);
 		#$this->view("show", $vars);
 	}
-
+*/
 }
